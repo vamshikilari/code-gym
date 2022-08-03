a819dcecb8b3a22c82307a73bb5f117939a72e9a
vamshikilari <vamshi9621@gmail.com>
2022-08-03 09:00:51 +0530

    super digit

---
+++ js/arrays_strings/superDigit/index.js
@@ -0,0 +1,16 @@
+function superDigit(num) {
+  let rem = num;
+  let sum = 0;
+  while (rem !== 0) {
+    sum += rem % 10;
+    rem /= 10;
+  }
+  if (sum >= 10) {
+    rem = sum;
+    return superDigit(rem);
+  } else {
+    return sum;
+  }
+}
+
+console.log(superDigit(1996));
